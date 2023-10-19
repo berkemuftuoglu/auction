06ae3d4e6821730f4b359536c202687368464589
Berke Muftuoglu <berkemuftuoglu1@gmail.com>
2023-10-19 22:46:54 +0100

    updated file

---
+++ sql/updated_create.sql
@@ -0,0 +1,44 @@
+CREATE TABLE Users (
+    user_id INT(11) AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    type BOOL, -- 0 for seller, 1 for buyer
+    first_name VARCHAR(255),
+    last_name VARCHAR(255),
+    UNIQUE(email)
+);
+
+CREATE TABLE Item (
+    item_id INT(11) AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    description VARCHAR(511),
+    reserve_price FLOAT(2),
+    starting_price FLOAT(2),
+    category ENUM('Electronics', 'Fashion', 'Home', 'Books', 'Other') NOT NULL,
+    photo MEDIUMBLOB --might need to change this
+);
+
+CREATE TABLE Auction (
+    auction_id INT(11) AUTO_INCREMENT PRIMARY KEY,
+    item_id INT(11),
+    start_time DATETIME NOT NULL,
+    end_time DATETIME NOT NULL,
+    FOREIGN KEY (item_id) REFERENCES Item(item_id)
+);
+
+CREATE TABLE Bids (
+    bid_id INT(11) AUTO_INCREMENT PRIMARY KEY,
+    auction_id INT(11) NOT NULL,
+    user_id INT(11) NOT NULL,
+    time_of_bid DATETIME NOT NULL,
+    FOREIGN KEY (auction_id) REFERENCES Auction(auction_id),
+    FOREIGN KEY (user_id) REFERENCES Users(user_id)
+);
+
+CREATE TABLE Watchlist (
+    user_id INT(11) NOT NULL,
+    item_id INT(11) NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES Users(user_id),
+    FOREIGN KEY (item_id) REFERENCES Item(item_id),
+    PRIMARY KEY (user_id, item_id)
+);
