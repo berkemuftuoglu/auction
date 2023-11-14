74ef6b89d0211536430db5a8df4102068b0d6e68
halaa901 <halahawashin18@gmail.com>
2023-11-14 22:00:24 +0000

    Work in Progress: TODO #2.

--- create_auction.php
+++ create_auction.php
@@ -24,33 +24,106 @@
       before they try to send it, but that kind of functionality should be
       extremely low-priority / only done after all database functions are
       complete. -->
+
       <form method="post" action="create_auction_result.php">
+
+      <!--****************************************************************-->
+      <!--********************** Auction Title ***************************-->
+      <!--****************************************************************-->
+
         <div class="form-group row">
           <label for="auctionTitle" class="col-sm-2 col-form-label text-right">Title of auction</label>
           <div class="col-sm-10">
-            <input type="text" class="form-control" id="auctionTitle" placeholder="e.g. Black mountain bike">
+            <input type="text" class="form-control" id="auctionTitle" name="auctionTitle" placeholder="e.g. Black mountain bike">
             <small id="titleHelp" class="form-text text-muted"><span class="text-danger">* Required.</span> A short description of the item you're selling, which will display in listings.</small>
           </div>
         </div>
+
+      <!--****************************************************************-->
+      <!--********************** Item Name *******************************-->
+      <!--****************************************************************-->
+
+      <div class="form-group row">
+          <label for="itemName" class="col-sm-2 col-form-label text-right">Name of Item</label>
+          <div class="col-sm-10">
+            <input type="text" class="form-control" id="itemName" name="itemName" placeholder="e.g. insert item name">
+            <small id="nameHelp" class="form-text text-muted"><span class="text-danger">* Required.</span> A name you wish to give your item.</small>
+          </div>
+      </div>
+
+      <!--****************************************************************-->
+      <!--********************** AuctionDetails *************************-->
+      <!--****************************************************************-->
+
         <div class="form-group row">
           <label for="auctionDetails" class="col-sm-2 col-form-label text-right">Details</label>
           <div class="col-sm-10">
-            <textarea class="form-control" id="auctionDetails" rows="4"></textarea>
+            <textarea class="form-control" id="auctionDetails" name="auctionDetails" rows="4"></textarea>
             <small id="detailsHelp" class="form-text text-muted">Full details of the listing to help bidders decide if it's what they're looking for.</small>
           </div>
         </div>
+
+      <!--****************************************************************-->
+      <!--********************** AuctionCategory *************************-->
+      <!--****************************************************************-->
+
         <div class="form-group row">
           <label for="auctionCategory" class="col-sm-2 col-form-label text-right">Category</label>
           <div class="col-sm-10">
-            <select class="form-control" id="auctionCategory">
-              <option selected>Choose...</option>
-              <option value="fill">Fill me in</option>
-              <option value="with">with options</option>
-              <option value="populated">populated from a database?</option>
+            <select class="form-control" id="auctionCategory" name="auctionCategory">
+              <option selected disabled>Choose...</option>
+              <option value="Electronics">Electronics</option>
+              <option value="Fashion">Fashion</option>
+              <option value="Home">Home</option>
+              <option value="Books">Books</option>
+              <option value="Other">Other</option>
             </select>
             <small id="categoryHelp" class="form-text text-muted"><span class="text-danger">* Required.</span> Select a category for this item.</small>
           </div>
         </div>
+
+      <!--****************************************************************-->
+      <!--********************** ItemColour ******************************-->
+      <!--****************************************************************-->
+
+      <div class="form-group row">
+          <label for="itemColour" class="col-sm-2 col-form-label text-right">Colour</label>
+          <div class="col-sm-10">
+              <select class="form-control" id="itemColour" name="itemColour">
+                  <option selected disabled>Choose...</option>
+                  <?php
+                  $colors = ['Red', 'Orange', 'Yellow', 'Green', 'Blue', 'Purple', 'Pink', 'White', 'Grey', 'Black', 'Brown', 'Other'];
+                  foreach ($colors as $color) {
+                      echo "<option value=\"$color\">$color</option>";
+                  }
+                  ?>
+              </select>
+              <small id="colourHelp" class="form-text text-muted"><span class="text-danger">* Required.</span> Select a colour for this item.</small>
+          </div>
+        </div>
+      
+      <!--****************************************************************-->
+      <!--********************** ItemConditiom ***************************-->
+      <!--****************************************************************--> 
+
+      <div class="form-group row">
+          <label for="itemCondition" class="col-sm-2 col-form-label text-right">Condition</label>
+          <div class="col-sm-10">
+              <select class="form-control" id="itemCondition" name="itemCondition">
+                  <option selected disabled>Choose...</option>
+                  <option value="Great">Great</option>
+                  <option value="Good">Good</option>
+                  <option value="Okay">Okay</option>
+                  <option value="Poor">Poor</option>
+              </select>
+              <small id="conditionHelp" class="form-text text-muted"><span class="text-danger">* Required.</span> Select a condition for this item.</small>
+          </div>
+      </div>
+
+      <!--****************************************************************-->
+      <!--********************** Starting Price **************************-->
+      <!--****************************************************************--> 
+
         <div class="form-group row">
           <label for="auctionStartPrice" class="col-sm-2 col-form-label text-right">Starting price</label>
           <div class="col-sm-10">
@@ -58,11 +131,16 @@
               <div class="input-group-prepend">
                 <span class="input-group-text">£</span>
               </div>
-              <input type="number" class="form-control" id="auctionStartPrice">
+              <input type="number" class="form-control" id="auctionStartPrice" name="auctionStartPrice">
             </div>
             <small id="startBidHelp" class="form-text text-muted"><span class="text-danger">* Required.</span> Initial bid amount.</small>
           </div>
         </div>
+
+      <!--****************************************************************-->
+      <!--********************** Reserve Price ***************************-->
+      <!--****************************************************************--> 
+
         <div class="form-group row">
           <label for="auctionReservePrice" class="col-sm-2 col-form-label text-right">Reserve price</label>
           <div class="col-sm-10">
@@ -70,25 +148,32 @@
               <div class="input-group-prepend">
                 <span class="input-group-text">£</span>
               </div>
-              <input type="number" class="form-control" id="auctionReservePrice">
+              <input type="number" class="form-control" id="auctionReservePrice" name="auctionReservePrice">
             </div>
             <small id="reservePriceHelp" class="form-text text-muted">Optional. Auctions that end below this price will not go through. This value is not displayed in the auction listing.</small>
           </div>
         </div>
+
+      <!--****************************************************************-->
+      <!--********************** End Date ********************************-->
+      <!--****************************************************************--> 
+
         <div class="form-group row">
           <label for="auctionEndDate" class="col-sm-2 col-form-label text-right">End date</label>
           <div class="col-sm-10">
-            <input type="datetime-local" class="form-control" id="auctionEndDate">
+            <input type="datetime-local" class="form-control" id="auctionEndDate" name="auctionEndDate">
             <small id="endDateHelp" class="form-text text-muted"><span class="text-danger">* Required.</span> Day for the auction to end.</small>
           </div>
         </div>
+
         <button type="submit" class="btn btn-primary form-control">Create Auction</button>
       </form>
     </div>
   </div>
 </div>
-
 </div>
+
+
 
 
 <?php include_once("footer.php")?>
--- create_auction_result.php
+++ create_auction_result.php
@@ -9,6 +9,8 @@
 /* TODO #1: Connect to MySQL database (perhaps by requiring a file that
             already does this). */
 
+    // For now, I will just set session variables and redirect.
+
 
 /* TODO #2: Extract form data into variables. Because the form was a 'post'
             form, its data can be accessed via $POST['auctionTitle'], 
@@ -17,8 +19,62 @@
             issue, give some semi-helpful feedback to user. */
 
 
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+        //*******************************************************************************/
+        // NOTE: Check for required fields to be not null & compare with not null values in the database!
+        // NOTE: Check that text fields only have text inside.
+        // NOTE: Fix echo message to be redirected onto the same page!
+        //******************************************************************************/
+
+        if (empty($_POST['auctionTitle']) || empty($_POST['itemName']) ||
+        empty($_POST['auctionCategory']) || empty($_POST['itemColour']) ||
+        empty($_POST['itemCondition']) || empty($_POST['auctionStartPrice']) ||
+        empty($_POST['auctionEndDate']) ) {
+                //$errorMessage = "Please fill in all required fields.";
+                echo "Error: Requiered fields is empty";
+        }
+    
+        else {
+            // Access the value of the data from the $_POST array
+            $name = $_POST['itemName'];
+            $description = $_POST['auctionDetails'];
+            $category = $_POST['auctionCategory'];
+            $colour = $_POST['itemColour'];
+            $condition = $_POST['itemCondition'];
+            $start_time = date("Y-m-d H:i:s"); // Get the current date and time;
+            $end_time = $_POST['auctionEndDate'];
+            $auction_title = $_POST['auctionTitle'];
+            $reserve_price = $_POST['auctionReservePrice'];
+            $starting_price = $_POST['auctionStartPrice'];
+
+            // Now $X contains the value entered in the 'X' field
+            echo "Name: " . $name . "<br>";
+            echo "Description: " . $description . "<br>";
+            echo "Category: " . $category . "<br>";
+            echo "Colour: " . $colour . "<br>";
+            echo "Condition: " . $condition . "<br>";
+            echo "Start Time: " . $start_time . "<br>";
+            echo "End Time: " . $end_time . "<br>";
+            echo "Auction Title: " . $auction_title . "<br>";
+            echo "Reserve Price: " . $reserve_price . "<br>";
+            echo "Starting Price: " . $starting_price . "<br>";
+        }
+
+    }
+
+
+
 /* TODO #3: If everything looks good, make the appropriate call to insert
             data into the database. */
+
+    // Retrieve data from the form
+
+    // Perform validation or additional processing if needed
+
+    // Insert data into the database
+    // $sql = "INSERT INTO users (username, password) VALUES ('$username', '$password')";
+
             
 
 // If all is successful, let user know.
