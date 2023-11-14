8721a02d41a5ff794f98ddfb729a7a1429ae1bea
halaa901 <halahawashin18@gmail.com>
2023-11-14 01:17:41 +0000

    dummy data commit

--- sql/dummy_data.sql
+++ sql/dummy_data.sql
@@ -1,27 +1,42 @@
--- Insert data into the Users table
-INSERT INTO Users (email, password, type, first_name, last_name) VALUES
-    ('buyer@example.com', 'password1', 1, 'John', 'Doe'),       -- Status: buyer
-    ('seller@example.com', 'password2', 0, 'Jane', 'Smith');    -- Status: seller
+-- Users Table
+INSERT INTO Users (email, password, type, first_name, last_name)
+VALUES 
+    ('john.doe@example.com', 'password123', 0, 'John', 'Doe'),
+    ('jane.smith@example.com', 'securepass', 1, 'Jane', 'Smith'),
+    ('sam.jones@example.com', 'samspassword', 1, 'Sam', 'Jones'),
+    ('sara.white@example.com', 'pass123', 0, 'Sara', 'White');
 
--- Insert data into the Item table
-INSERT INTO Item (name, description, reserve_price, starting_price, category, status) VALUES
-    ('Smartphone', 'Brand new smartphone', 500.00, 300.00, 'Electronics', 0),       -- Status: available
-    ('Laptop', 'Used laptop in good condition', 800.00, 600.00, 'Electronics', 0),  -- Status: available
-    ('Designer Dress', 'High-end fashion dress', 1000.00, 700.00, 'Fashion', 1);    -- Status: sold
+-- Item Table
+INSERT INTO Item (name, description, category, colour, condition, photo)
+VALUES 
+    ('Smartphone', 'Brand new, in original packaging', 'Electronics', 'Black', 'Great', '/photos/smartphone.jpg'),
+    ('Vintage Bookshelf', 'Antique wooden bookshelf', 'Home', 'Brown', 'Good', '/photos/bookshelf.jpg'),
+    ('Designer Dress', 'Elegant evening dress', 'Fashion', 'Red', 'Great', '/photos/dress.jpg'),
+    ('Coffee Table', 'Modern glass coffee table', 'Home', 'White', 'Okay', '/photos/coffee_table.jpg');
 
--- Insert data into the Auction table
-INSERT INTO Auction (item_id, start_time, end_time, auction_title) VALUES
-    (1, '2023-10-25 10:00:00', '2023-10-26 10:00:00', 'Smartphone Auction'),
-    (2, '2023-10-26 14:00:00', '2023-10-27 14:00:00', 'Laptop Auction'),
-    (3, '2023-10-27 12:00:00', '2023-10-28 12:00:00', 'Designer Dress Auction');
+-- Auction Table
+INSERT INTO Auction (item_id, start_time, end_time, auction_title, reserve_price, starting_price)
+VALUES 
+    (1, '2023-11-01 12:00:00', '2023-11-10 18:00:00', 'Latest Smartphone Auction', 500.00, 200.00),
+    (2, '2023-11-05 14:00:00', '2023-11-15 20:00:00', 'Vintage Bookshelf Sale', 150.00, 50.00),
+    (3, '2023-11-08 10:00:00', '2023-11-18 15:00:00', 'Designer Dress Auction', 300.00, 100.00),
+    (4, '2023-11-12 09:00:00', '2023-11-22 12:00:00', 'Modern Coffee Table Bidding', 200.00, 80.00);
 
--- Insert data into the Bids table
-INSERT INTO Bids (auction_id, user_id, time_of_bid, price) VALUES
-    (1, 1, '2023-10-25 10:30:00', 320.00),
-    (1, 2, '2023-10-25 10:45:00', 350.00),
-    (2, 1, '2023-10-26 14:15:00', 620.00);
+-- Bids Table
+INSERT INTO Bids (auction_id, user_id, time_of_bid, price)
+VALUES 
+    (1, 2, '2023-11-05 14:30:00', 220.00),
+    (1, 4, '2023-11-07 16:45:00', 250.00),
+    (2, 3, '2023-11-10 10:20:00', 60.00),
+    (3, 1, '2023-11-15 12:10:00', 150.00),
+    (4, 2, '2023-11-18 11:30:00', 90.00);
 
--- Insert data into the Watchlist table
-INSERT INTO Watchlist (user_id, item_id) VALUES
+-- Watchlist Table
+INSERT INTO Watchlist (user_id, item_id)
+VALUES 
     (1, 2),
-    (2, 1);
+    (2, 3),
+    (3, 1),
+    (4, 4),
+    (1, 3),
+    (2, 4);
