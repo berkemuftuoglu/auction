821cd6bc1284ac704799ceabddf29c87c748f2f5
Berke Muftuoglu <berkemuftuoglu1@gmail.com>
2023-11-21 12:29:04 +0000

    login done.

--- login_result.php
+++ login_result.php
@@ -1,4 +1,5 @@
 <?php
+require_once('database.php');
 
 // TODO: Extract $_POST variables, check they're OK, and attempt to login.
 // Notify user of success/failure and redirect/give navigation options.
@@ -6,13 +7,35 @@
 // For now, I will just set session variables and redirect.
 
 session_start();
-$_SESSION['logged_in'] = true;
-$_SESSION['username'] = "test";
-$_SESSION['account_type'] = "buyer";
 
-echo('<div class="text-center">You are now logged in! You will be redirected shortly.</div>');
+// Create database connection
+$connection = db_connect();
 
-// Redirect to index after 5 seconds
-header("refresh:5;url=index.php");
+// Extract $_POST variables
+$username = $_POST['username'];
+$password = $_POST['password'];
 
+// Prepare SQL query
+$query = "SELECT * FROM users WHERE username = '$username' AND password = '$password'";
+$result = mysqli_query($connection, $query);
+confirm_result_set($result);
+
+if (mysqli_num_rows($result) === 1) {
+    // Login successful
+    $_SESSION['logged_in'] = true;
+    $_SESSION['username'] = $username;
+
+    // TODO: Set 'account_type' or other session variables as needed
+    $_SESSION['account_type'] = "buyer";
+
+    echo('<div class="text-center">You are now logged in! You will be redirected shortly.</div>');
+    header("refresh:5;url=index.php");
+} else {
+    // Login failed
+    echo('<div class="text-center">Login failed. Invalid username or password.</div>');
+}
+
+// Free result set and close database connection
+mysqli_free_result($result);
+db_disconnect($connection);
 ?>
