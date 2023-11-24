6e60ab653efcad1f52941bc3dde1b9e71bbe21c8
sarahfairbro <sarah.fairbrother@rogers.com>
2023-11-24 11:36:11 +0000

    fixing account type session variables

--- header.php
+++ header.php
@@ -54,7 +54,7 @@
       <a class="nav-link" href="browse.php">Browse</a>
     </li>
 <?php
-  if (isset($_SESSION['account_type']) && $_SESSION['account_type'] == 'buyer') {
+  if (isset($_SESSION['account_type']) && $_SESSION['account_type'] == '1') {
   echo('
 	<li class="nav-item mx-1">
       <a class="nav-link" href="mybids.php">My Bids</a>
@@ -63,7 +63,7 @@
       <a class="nav-link" href="recommendations.php">Recommended</a>
     </li>');
   }
-  if (isset($_SESSION['account_type']) && $_SESSION['account_type'] == 'seller') {
+  if (isset($_SESSION['account_type']) && $_SESSION['account_type'] == '0') {
   echo('
 	<li class="nav-item mx-1">
       <a class="nav-link" href="mylistings.php">My Listings</a>
